88bcc8550cac2b0cc17cad7f73c1814dda9d26a6
vculas <vinaculasvc@gmail.com>
2021-03-25 16:51:41 -0700

    testing

---
+++ Calculator/functions.py
@@ -0,0 +1,10 @@
+# Author: Vina Culas
+# Date: 3/25/21
+# File: functions.py
+# Description: This file is the function library for main_calc.py
+
+def test():
+    print("The import worked!")
+
+def div(num, den):
+    return (num // den)
---
+++ Calculator/main_calc.py
@@ -0,0 +1,23 @@
+# Author: Vina Culas
+# Date: 3/25/21
+# File: main_calc.py
+# Description: This file is a calculator
+
+import sys
+import functions
+
+print("VINA'S CALCULATOR\n")
+
+eq = input("Input Your Equation:\n")
+print("%s" % eq)
+parsed = eq.split()
+
+number = div(6, 3)
+print(number)
+#sum = int(parsed[2])
+
+#sum += 4
+
+#print(sum)
+
+
